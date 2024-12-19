ddbc8b6161ebdb4ac3a6a36ab78bd097c58a5d33
hector <hectorbeltrancifuentes13@gmail.com>
2024-12-19 17:55:42 -0500

    Banner preeliminar

--- src/Pages/inicio.jsx
+++ src/Pages/inicio.jsx
@@ -46,57 +46,72 @@ export default function Inicio() {
                 </section>
 
             </div>
-
+                    {/**Sole Section Medium */}
             <div className="soleSection">
                 <section className="">
                     <h1>Elige la que se adapte a ti</h1>
                     <p>Personaliza tus zapatillas elige colores, detalles y acabados que reflejen tu futbol y personalidad</p>
                     {/**SLIDER WIHT SWIPER "INSTALL SWIPER IS MANDATORY" */}
-                        <div className="sliderContainer">
+                    <div className="sliderContainer">
                         <Slider />
-                        </div>
-                        
+                    </div>
 
+                    <Button
+                        text="Ver más"
+                        style={{
+                            backgroundColor: "#5759E5",
+                            color: "#FFFFFF",
+                            border: "none",
+                            width:"15%",
+                            height: "8vh"
+
+                        }}
+                    />
 
 
 
                 </section>
             </div>
 
+
+
+
+
+            {/**Banner section */}
             <div className="bannerSection">
                 <div className="container">
-                <section className="ContactUs">
+                    <section className="ContactUs">
                         <h2>Contáctanos</h2>
                         <p>zapatillas@gmail.com</p>
                         <p>+57 3196164030</p>
-                </section>
-                <section className="Politics">
+                    </section>
+                    <section className="Politics">
                         <h2>Políticas</h2>
                         <a href=""><p>Política Envíos</p></a>
                         <a href=""><p>Política Cambios y Devoluciones</p></a>
                         <a href=""><p>Política de Uso de Cookies</p></a>
                         <a href=""><p>Política de Privacidad</p></a>
                         <a href=""><p>Términos y Condiciones</p></a>
-                </section>
-                <section className="Suscription">
-                    <h2>Suscribirse</h2>
-                    <input type="email" name="email" id="email" placeholder="Correo Electronico"/>
-                    <Button
-                    text="Enviar"
-                    style={{
-                        backgroundColor: "#5759e573",
-                        color: "#fff",
-                        borderColor: "#fff",
-                        width: "40%",
-                        borderRadius: "6px"
-                        
+                    </section>
+                    <section className="Suscription">
+                        <h2>Suscribirse</h2>
+                        <input type="email" name="email" id="email" placeholder="Correo Electronico" />
+                        <Button
+                            text="Enviar"
+                            style={{
+                                backgroundColor: "#5759e573",
+                                color: "#fff",
+                                borderColor: "#5759E5",
+                                width: "40%",
+                                borderRadius: "6px"
 
 
-                    }}
-                    />
 
-                </section>
-                
+                            }}
+                        />
+
+                    </section>
+
                 </div>
                 <div className="creditos">
                     <p>2024 Zapatillas Hat Trick todos los derechos reservados. Desarrollado por: Héctor Beltran</p>
